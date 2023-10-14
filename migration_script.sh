5e412d0f6ac684622091872a9a04b84247446e27
HuyNg <elvizhuy@gmail.com>
2023-10-14 22:31:55 +0700

    .

--- migration_script.sh
+++ migration_script.sh
@@ -1,2 +1,19 @@
 #! /bin/bash
 
+# Thông tin cơ sở dữ liệu MySQL
+db_user="huynn"
+db_password="123123"
+db_name="test_mysql_db"
+
+# Đường dẫn đến tệp dump
+dump_file="/home/isofh/dumpdb.sql"
+
+# migrate dữ liệu
+mysql -u $db_user -p$db_password $db_name < $dump_file
+
+# Kiểm tra kết quả
+if [ $? -eq 0 ]; then
+  echo "Migration successful."
+else
+  echo "Migration failed."
+fi
